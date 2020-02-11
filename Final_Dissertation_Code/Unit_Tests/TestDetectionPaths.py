3f0765c40cdb53bf1a04069ea9b3dc87f3a042d3
jennifernolan <j-nolan77@hotmail.com>
2020-02-11 16:50:32 +0000

    Initial unit tests conducted

--- Final_Dissertation_Code/Unit_Tests/TestDetectionPaths.py
+++ Final_Dissertation_Code/Unit_Tests/TestDetectionPaths.py
@@ -2,14 +2,17 @@
 import unittest
 import os
 
-from models.research.object_detection.ObjectDetection import path_to_graph
-from models.research.object_detection.ObjectDetection import path_to_labels
+from ObjectDetection import path_to_graph
+from ObjectDetection import path_to_labels
+from ObjectDetection import model_name
 
-os_path = os.getcwd()
-path = os.path.join(os_path, 'models/research/object_detection')
-test_graph_path = os.path.join(path, path_to_graph)
-test_labels_path = os.path.join(path, path_to_labels)
+path = os.getcwd()
 
+incorrect_graph_path = os.path.join(path, 'frozen_inference_graph.pb')
+incorrect_labels_path = os.path.join(path, 'mscoco_label_map.pbtxt')
+
+test_graph_path = os.path.join(path, model_name, 'frozen_inference_graph.pb')
+test_label_path = os.path.join(path, 'data', 'mscoco_label_map.pbtxt')
 
 class TestDetectionPaths(unittest.TestCase):
     
@@ -19,17 +22,40 @@ class TestDetectionPaths(unittest.TestCase):
     def tearDown(self):
         pass
     
+    
+    def test_incorrect_graph_path(self):
+        test = incorrect_graph_path == path_to_graph
+        self.assertFalse(test)
+    
     def test_part_graph_file_path(self):
-        self.assertIn('frozen_inference_graph', test_graph_path)
+        self.assertIn('frozen_inference_graph', path_to_graph)
         
     def test_full_graph_file_path(self):
         self.assertEqual(test_graph_path, path_to_graph)
         
+    def test_full_graph_upper(self):
+        self.assertFalse(path_to_graph.isupper())
+        
+    def test_full_graph_lower(self):
+        self.assertFalse(path_to_graph.islower())    
+        
+        
+        
+    def test_incorrect_labels_path(self):
+        test = incorrect_labels_path == path_to_graph
+        self.assertFalse(test)
+    
     def test_part_labels_file_path(self):
-        self.assertIn('mscoco_label_map', test_labels_path)
+        self.assertIn('mscoco_label_map', path_to_labels)
         
     def test_full_labels_file_path(self):
-        self.assertEqual(test_labels_path, path_to_labels)
+        self.assertEqual(test_label_path, path_to_labels)
+        
+    def test_full_labels_upper(self):
+        self.assertFalse(path_to_labels.isupper())
+        
+    def test_full_labels_lower(self):
+        self.assertFalse(path_to_labels.islower())  
 
     
 if __name__ == '__main__':
