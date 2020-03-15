cbbe24803119fde6bf50285bec8a97b434361c8e
jennifernolan <j-nolan77@hotmail.com>
2020-03-15 15:36:24 +0000

    unittest conducted for distance sensor

---
+++ Final_Dissertation_Code/Unit_Tests/TestSensorDistance.py
@@ -0,0 +1,40 @@
+#Python code to test distance detection
+import unittest
+from RangeSensor import get_distance
+
+class TestSensorDistance(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_long_distance_sensor(self):
+        distance = get_distance()
+        print(distance)
+        if distance >= 60:
+            self.assertTrue(distance >= 60)
+        else:
+            self.assertFalse(distance >= 60)
+        
+    def test_medium_distance_sensor(self):
+        distance = get_distance()
+        print(distance)
+        if distance < 60 and distance >= 20:
+            self.assertTrue(distance < 60 and distance >= 20)
+        else:
+            self.assertFalse(distance < 60 and distance >= 20)
+        
+        
+    def test_short_distance_sensor(self):
+        distance = get_distance()
+        print(distance)
+        if distance < 20:
+            self.assertTrue(distance < 20)
+        else:
+            self.assertFalse(distance < 20)
+        
+    
+if __name__ == '__main__':
+    unittest.main()
